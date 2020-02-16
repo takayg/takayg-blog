bc371aedfc70188e0cc2348b8f5a0983d7ffda1e
Your Name <you@example.com>
2020-02-17 06:53:40 +0900

    change

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+from markdownx.admin import MarkdownxModelAdmin
+
+admin.site.register(Post, MarkdownxModelAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,16 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdownify
+
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    explanation = models.CharField(max_length=200, null=True)
+    # text = models.TextField()
+    text = MarkdownxField('Contents', help_text='To Write with Markdown format')
+    
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
@@ -16,3 +21,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def formatted_markdown(self):
+        return markdownify(self.text)
