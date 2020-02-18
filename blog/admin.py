a0694860e0a6bda8627617c9da333afba58ef666
Your Name <you@example.com>
2020-02-18 15:52:59 +0900

    キャッシュ削除

--- blog/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-from markdownx.admin import MarkdownxModelAdmin
-
-admin.site.register(Post, MarkdownxModelAdmin)
--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'blog'
-urlpatterns = [
-    path('', views.IndexView.as_view(), name="index"),
-    path('blog_list/', views.BlogListView.as_view(), name="blog_list"),
-    path('blog_detail/<int:pk>/', views.BlogDetailView.as_view(), name="blog_detail"),
-]
